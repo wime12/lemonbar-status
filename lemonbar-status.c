586706c4d962231ae6969eaf60da04036784a2f4
Wilfried Meindl <wilfried.meindl@gmail.com>
2017-12-30 15:59:28 +0100

    add mail and let format_* function return the string

--- lemonbar-status.c
+++ lemonbar-status.c
@@ -32,6 +32,7 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/un.h>
 #include <machine/apmvar.h>
 /* room for net includes */
@@ -50,7 +51,10 @@
 
 static int	open_socket(const char *);
 static void	get_battery_info(struct apm_power_info *);
-static void	format_battery_info(char *, size_t, struct apm_power_info *);
+static char    *format_battery_info(struct apm_power_info *);
+static void	get_mail_info(int fd, struct stat *);
+static char    *format_mail_info(struct stat *);
+static int	timespec_later(struct timespec *, struct timespec *);
 
 
 static int
@@ -92,44 +96,83 @@ get_battery_info(struct apm_power_info *info)
 	if (state < 0) err(1, "cannot read battery info");
 }
 
-static void
-format_battery_info(char *str, size_t len, struct apm_power_info *info)
+static char *
+format_battery_info(struct apm_power_info *info)
 {
 	int minutes, n;
+	static char str[BATT_INFO_BUFLEN];
 
 	n = -1;
 	switch (info->ac_state) {
 	case APM_AC_OFF:
 	        minutes = info->minutes_left;
 		if (minutes < 0)
-			n = strlcpy(str, "??:??", len);
+			n = strlcpy(str, "??:??", BATT_INFO_BUFLEN);
 		else
-			n = snprintf(str, len, "%d:%02d",
+			n = snprintf(str, BATT_INFO_BUFLEN, "%d:%02d",
 			    minutes / 60, minutes % 60);
 		/* FALLTHROUGH */
 	case APM_AC_ON:
 		if (n < 0)
-			n = strlcpy(str, "A/C", len);
+			n = strlcpy(str, "A/C", BATT_INFO_BUFLEN);
 
-		snprintf(str + n, len - n, " (%d%%)", info->battery_life);
+		snprintf(str + n, BATT_INFO_BUFLEN - n, " (%d%%)",
+		    info->battery_life);
+		return str;
 		break;
 	default:
-		snprintf(str, len, "???");
+		return NULL;
 	}
 }
 
 
-/* Network */
+/* Mail */
+
+static void
+get_mail_info(int fd, struct stat *mail_info)
+{
+	if (fstat(fd, mail_info) < 0)
+		err(1, "cannot get mail file status");
+}
+
+
+/* Was t1 was later than t2? */
+static int
+timespec_later(struct timespec *t1, struct timespec *t2)
+{
+	if (t1->tv_sec == t2->tv_sec)
+		return t1->tv_nsec > t2->tv_nsec;
+	else
+		return t1->tv_sec > t2->tv_sec;
+}
+
+static char *
+format_mail_info(struct stat *mail_info)
+{
+
+	if (timespec_later(&mail_info->st_mtim, &mail_info->st_atim))
+		return "MAIL";
+	else
+		return NULL;
+}
 
 int
 main()
 {
+	int mail_fd;
     	struct apm_power_info batt_info;
-	char batt_string[BATT_INFO_BUFLEN];
+	struct stat mail_info;
+	char *mail_msg;
 
 	get_battery_info(&batt_info);
-	format_battery_info(batt_string, BATT_INFO_BUFLEN, &batt_info);
-	puts(batt_string);
+	puts(format_battery_info(&batt_info));
+
+	if ((mail_fd = open("/var/mail/wilfried", O_RDONLY)) < 0)
+		err(1, "cannot open /var/mail/wilfried");
+	get_mail_info(mail_fd, &mail_info);
+	close(mail_fd);
+	mail_msg = format_mail_info(&mail_info);
+	puts(mail_msg == NULL ? "NO MAIL" : mail_msg);
 
 	return 0;
 }
